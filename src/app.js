1b36fb99ac6fa98d2b1df8178566758f61b9b817
Joe-noh <goflb.jh@gmail.com>
2015-02-05 03:08:52 +0900

    move sidebar to app view

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
 import {Router} from 'aurelia-router';
+import {BeersApi} from './beers-api'
 
 export class App {
   static inject() {
-    return [Router];
+    return [Router, BeersApi];
   }
 
-  constructor(router) {
+  constructor(router, beersApi) {
     this.router = router;
     this.router.configure(config => {
       config.title = 'Aurelia Beer';
       config.map([
-        {route: ['', 'beers'], moduleId: 'beers', nav: true},
-        {route: 'beers/:id',   moduleId: 'beer'}
+        {route: '',          moduleId: 'home', nav: true},
+        {route: 'beers/:id', moduleId: 'beer'}
       ]);
     });
+
+    this.beersApi = beersApi;
+  }
+
+  activate() {
+    this.beers = this.beersApi.fetchAll();
   }
 }
